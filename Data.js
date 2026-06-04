c59e984cf72972f4e4eb214384e20eb6470e8809
Mustafa007gt <127961278+Mustafa007gt@users.noreply.github.com>
2026-06-04 21:13:06 +0300

    Add games data to Data.js
    Added an array of game objects with details.

--- Data.js
+++ Data.js
@@ -1,1 +1,20 @@
-
+const games = [
+  {
+    id: 1,
+    name: "PUBG Mobile",
+    category: "action",
+    image: "images/pubg.jpg",
+    size: "1.5GB",
+    type: "Online",
+    link: "https://t.me/yourchannel"
+  },
+  {
+    id: 2,
+    name: "Asphalt 9",
+    category: "racing",
+    image: "images/asphalt.jpg",
+    size: "2GB",
+    type: "Offline",
+    link: "https://t.me/yourchannel"
+  }
+];
